6ea56c48fa4957beff7b20d07c4e16408466d1d5
pduykhanh <pduykhanh@vnpt.vn>
2022-11-15 17:28:03 +0700

    update

--- routes/content2.js
+++ routes/content2.js
@@ -1,29 +1,40 @@
 const express = require('express');
 
+const { route } = require('express/lib/application');
+
 const routerContent2 = express.Router();
 
-routerContent2.get('/content2',function(req, res) {
+const controller = require('../controller/web/controller');
+const router = express.Router();
+
+routerContent2.get('/content2', function (req, res) {
     res.send('hello content2');
 })
 
-routerContent2.get('/content3',function(req, res) {
+routerContent2.get('/content3', function (req, res) {
     res.send('hello content3');
 })
 
-routerContent2.get('/content/:age-:gender',function(req, res) {
+routerContent2.get('/content/:age-:gender', function (req, res) {
     res.send(req.params.age);
 })
 
-routerContent2.get('/multi-function',func1,[func1,func2]);
+routerContent2.get('/content/*', function (req, res) {
+    res.send('HIHIHIHI *');
+})
 
-function func1(req, res , next) {
+routerContent2.get('/multi-function', func1, [func1, func2]);
+
+function func1(req, res, next) {
     console.log('function1');
     next();
 }
 
-function func2(req, res , next) {
+routerContent2.get('/xnxx',controller.getIndex);
+
+function func2(req, res, next) {
     console.log('function2');
-    
+
 }
 
 module.exports.routerContent2 = routerContent2;
